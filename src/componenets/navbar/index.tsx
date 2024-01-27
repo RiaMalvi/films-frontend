20b5c9f49b916587df519e44363a504534361183
ria <lci2021052@iiitl.ac.in>
2024-01-27 21:53:51 +0530

    bg changed

--- src/componenets/navbar/index.tsx
+++ src/componenets/navbar/index.tsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <nav className="bg-transparent absolute top-0 left-0 right-0">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <span className="self-center text-4xl font-bold text-red-600">FilmyDuniya</span>
+          <span className="self-center text-4xl font-bold text-red-600 drop-shadow-xl">FilmyDuniya</span>
         <div className="flex md:order-2">
           <button type="button" data-collapse-toggle="navbar-search" aria-controls="navbar-search" aria-expanded="false" className="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 me-1">
             <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -5,12 +5,12 @@ const Home = () => {
   return (
     <div>
       <div className="flex h-screen justify-center items-center flex-col">
-        <div className="w-full h-screen bg-[url('../assets/img/collage.jpg')] bg-cover bg-center">
+        <div className="w-full h-screen bg-[url('../assets/img/bg2.jpg')] bg-cover bg-center">
         <div className="w-full h-full flex  justify-center items-center backdrop-brightness-50">
         <Navbar/>
         <div className="flex flex-col justify-center items-center">
           <h1 className="text-6xl text-white font-bold">Welcome to FilmyDuniya</h1>
-          <p className="text-3xl text-white font-bold">The best place to review movies</p>
+          <p className="text-2xl text-white my-5">The Best Place to Review Movies</p>
           </div>
         </div>
         </div>
