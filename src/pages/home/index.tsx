4e56fdc144c92e134f0927457efcd358bf2e2247
ria <lci2021052@iiitl.ac.in>
2024-01-27 18:53:39 +0530

    home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -8,6 +8,10 @@ const Home = () => {
         <div className="w-full h-screen bg-[url('../assets/img/collage.jpg')] bg-cover bg-center">
         <div className="w-full h-full flex  justify-center items-center backdrop-brightness-50">
         <Navbar/>
+        <div className="flex flex-col justify-center items-center">
+          <h1 className="text-6xl text-white font-bold">Welcome to FilmyDuniya</h1>
+          <p className="text-3xl text-white font-bold">The best place to review movies</p>
+          </div>
         </div>
         </div>
       </div>
