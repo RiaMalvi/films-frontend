5692bdcd6c8f7e38f7c2fabbe2de69e8fe5a298c
ria <lci2021052@iiitl.ac.in>
2024-01-27 16:47:10 +0530

    changes

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,12 @@
+import Carousel from '@/componenets/atoms/carousel'
+import Navbar from '@/componenets/navbar'
 import React from 'react'
 
 const Home = () => {
   return (
     <div>
-      
+      <Navbar/>
+      <Carousel/>
     </div>
   )
 }
