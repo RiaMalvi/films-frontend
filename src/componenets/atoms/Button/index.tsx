1392c1df2537fc8789fe95a446ded7f22c51d3b7
ria <lci2021052@iiitl.ac.in>
2024-01-27 18:46:38 +0530

    bg

---
+++ src/componenets/atoms/Button/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Button = () => {
+  return (
+    <button className='bg-red-600 rounded-md'></button>
+  )
+}
+
+export default Button
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,7 +4,13 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      <Navbar/>
+      <div className="flex h-screen justify-center items-center flex-col">
+        <div className="w-full h-screen bg-[url('../assets/img/collage.jpg')] bg-cover bg-center">
+        <div className="w-full h-full flex  justify-center items-center backdrop-brightness-50">
+        <Navbar/>
+        </div>
+        </div>
+      </div>
     </div>
   )
 }
