2b34971440d2b71a3de67e6bcaefd1eaba2890b9
ria <lci2021052@iiitl.ac.in>
2024-01-29 00:12:31 +0530

    changes

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,18 @@
+import Navbar from '@/componenets/navbar'
+import React from 'react'
+import bg1 from '@/assets/img/bg1.jpg'
+
+const Login = () => {
+  return (
+    <div className="w-full h-screen bg-[url('../assets/img/bg1.jpg')] bg-cover bg-center">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <span className="self-center text-4xl font-bold text-red-600 drop-shadow-xl">FilmyDuniya</span>
+      </div>
+      <div className="w-full h-full flex  justify-center items-center backdrop-brightness-50">
+        
+      </div>
+    </div>
+  )
+}
+
+export default Login
