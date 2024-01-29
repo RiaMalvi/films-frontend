d66f906c8e370b7bc2c8a272f8666e110859c488
ria <lci2021052@iiitl.ac.in>
2024-01-29 13:35:20 +0530

    added login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,16 +1,17 @@
-import Navbar from '@/componenets/navbar'
 import React from 'react'
-import bg1 from '@/assets/img/bg1.jpg'
+import Footer from '@/componenets/footer'
+import Form from '@/componenets/Form'
 
 const Login = () => {
   return (
-    <div className="w-full h-screen bg-[url('../assets/img/bg1.jpg')] bg-cover bg-center">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <span className="self-center text-4xl font-bold text-red-600 drop-shadow-xl">FilmyDuniya</span>
+    <div className="w-full h-screen bg-[url('../assets/img/bg1.jpg')] bg-cover bg-center flex flex-col">
+      <div className="w-full h-screen flex flex-col backdrop-brightness-50 items-center">
+        <div className="self-start text-4xl font-bold text-red-600 drop-shadow-xl p-4 mx-12 mt-2">FilmyDuniya</div>
+        <div className="p-8 ">
+          <Form />
+        </div>
       </div>
-      <div className="w-full h-full flex  justify-center items-center backdrop-brightness-50">
-        
-      </div>
+      <Footer />
     </div>
   )
 }
