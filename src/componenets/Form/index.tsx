6fdfbad6f8b4372bfd8da82d4cbf68f98fb9f296
ria <lci2021052@iiitl.ac.in>
2024-01-29 15:45:45 +0530

    added login page

--- src/componenets/Form/index.tsx
+++ src/componenets/Form/index.tsx
@@ -3,14 +3,19 @@ import Button from '../atoms/button'
 
 const Form = () => {
   return (
-    <div className="bg-black text-white flex flex-col p-12 bg-opacity-70 rounded-md gap-6 w-[150%]">
+    <div className="bg-black text-white flex flex-col p-12 bg-opacity-70 rounded-md gap-6 w-96">
       <h1 className="text-3xl font-bold">Sign In</h1>
       <div className="flex flex-col gap-3 items-center">
         <input type="text" placeholder="Email or Phone Number" className="bg-transparent border-gray-700 border-2 p-2 w-full rounded-md py-3"></input>
         <input type="password" placeholder="Password" className="bg-transparent border-gray-700 border-2 p-2 w-full rounded-md py-3"></input>
         <Button className="text-white bg-red-700 hover:bg-red-800 px-5 py-2.5 dark:bg-red-600 dark:hover:bg-red-700 text-sm !rounded-md w-full">Sign In</Button>
         <p className="text-white text-sm">Forgot Password?</p>
-        <input type="checkbox" className="bg-black border-gray-700" ></input>
+        <div className="flex gap-2 self-start mt-10">
+          <input type="checkbox" className="bg-black border-gray-700 "></input>
+          <p className="text-white text-sm">Remember Me</p>
+        </div>
+        <p className="text-gray-400 self-start text-sm">New to Netflix? <span className="font-semibold text-white text-sm">Sign Up Now.</span></p>
+        <p className="text-gray-500 text-xs">This page is protected by Google reCAPTCHA to ensure you're not a bot. <span className="text-blue-600 text-xs">Learn more.</span></p>
       </div>
     </div>
   )
