0f550499fe2591f9e5fa2b241e5cd0b27986adc8
ria <lci2021052@iiitl.ac.in>
2024-02-01 00:00:44 +0530

    changes

--- src/componenets/SignUpForm/index.tsx
+++ src/componenets/SignUpForm/index.tsx
@@ -1,17 +1,61 @@
 import React from 'react'
 import Button from '../atoms/button'
+import { useForm } from 'react-hook-form'
+import { toast } from 'react-toastify'
+import axios from 'axios'
+
 
 const SForm = () => {
+  const [formState, setFormState] = React.useState({
+    username: '',
+    name: '',
+    email: '',
+    phone: '',
+    password: ''
+  })
+  const [cpasswd, setCPasswd] = React.useState('');
+
+  const handleSubmit = () => {
+    if (formState.password != cpasswd) {
+      toast.error('Password does not match', {
+        position: 'top-right',
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        draggable: false
+      })
+      return
+    }
+    axios.post('http://localhost:1337/api/auth/local/register', formState).then((res) => {
+      toast.success("Account Created Successfully")
+    }).catch((err) => {
+      toast.error("Error Occured")
+    })
+  }
+
   return (
     <div className="bg-black text-white flex flex-col p-12 bg-opacity-70 rounded-md gap-6 w-96">
       <h1 className="text-3xl font-bold">Sign Up</h1>
       <div className="flex flex-col gap-3 items-center">
-        <input type="text" placeholder="Name" className="bg-transparent border-gray-700 border-2 p-2 w-full rounded-md py-3"/>
-        <input type="text" placeholder="Phone Number" className="bg-transparent border-gray-700 border-2 p-2 w-full rounded-md py-3" />
-        <input type="email" placeholder="Email" className="bg-transparent border-gray-700 border-2 p-2 w-full rounded-md py-3"/>
-        <input type="password" placeholder="Password" className="bg-transparent border-gray-700 border-2 p-2 w-full rounded-md py-3"/>
-        <input type="password" placeholder="Confirm Password" className="bg-transparent border-gray-700 border-2 p-2 w-full rounded-md py-3"/>
-        <Button className="text-white bg-red-700 hover:bg-red-800 px-5 py-2.5 dark:bg-red-600 dark:hover:bg-red-700 text-sm !rounded-md w-full">Sign Up</Button>
+        <input type="text" placeholder="Username" className="bg-transparent border-gray-700 border-2 p-2 w-full rounded-md py-3" value={formState.username} onChange={(event) => {
+          setFormState({ ...formState, username: event.target.value })
+        }} />
+        <input type="text" placeholder="Name" className="bg-transparent border-gray-700 border-2 p-2 w-full rounded-md py-3" value={formState.name} onChange={(event) => {
+          setFormState({ ...formState, name: event.target.value })
+        }} />
+        <input type="text" placeholder="Phone Number" className="bg-transparent border-gray-700 border-2 p-2 w-full rounded-md py-3" value={formState.phone} onChange={(e) => {
+          setFormState({ ...formState, phone: e.target.value })
+        }} />
+        <input type="email" placeholder="Email" className="bg-transparent border-gray-700 border-2 p-2 w-full rounded-md py-3" value={formState.email} onChange={(event) => {
+          setFormState({ ...formState, email: event.target.value })
+        }} />
+        <input type="password" placeholder="Password" className="bg-transparent border-gray-700 border-2 p-2 w-full rounded-md py-3" value={formState.password} onChange={(e) => {
+          setFormState({ ...formState, password: e.target.value })
+        }} />
+        <input type="password" placeholder="Confirm Password" className="bg-transparent border-gray-700 border-2 p-2 w-full rounded-md py-3" value={cpasswd} onChange={(event) => {
+          setCPasswd(event.target.value)
+        }} />
+        <Button className="text-white bg-red-700 hover:bg-red-800 px-5 py-2.5 dark:bg-red-600 dark:hover:bg-red-700 text-sm !rounded-md w-full" onClick={handleSubmit}>Sign Up</Button>
         <div className="flex gap-2 self-start mt-10">
           <input type="checkbox" className="bg-black border-gray-700 "></input>
           <p className="text-white text-sm">Remember Me</p>
