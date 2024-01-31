82ed1337c942357a748dfdc6a02332490c3d2dcf
ria <lci2021052@iiitl.ac.in>
2024-01-31 17:36:28 +0530

    modified signup form

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -4,8 +4,8 @@ import Form from '@/componenets/SignUpForm'
 
 const SignUp = () => {
   return (
-    <div className="w-full h-screen bg-[url('../assets/img/bg1.jpg')] bg-cover bg-center flex flex-col">
-      <div className="w-full h-screen flex flex-col backdrop-brightness-50 items-center">
+    <div className="w-full min-h-screen bg-[url('../assets/img/bg1.jpg')] bg-cover bg-center flex flex-col">
+      <div className="w-full min-h-screen flex flex-col backdrop-brightness-50 items-center">
         <div className="self-start text-4xl font-bold text-red-600 drop-shadow-xl p-4 mx-12 mt-2">FilmyDuniya</div>
         <div className="p-8">
           <Form />
